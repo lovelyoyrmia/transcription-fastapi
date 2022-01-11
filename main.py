9de9cfbefa094571c018605131c35308f1d68507
lovelyoyrmia <mokalulovelyo@gmail.com>
2022-01-11 14:12:18 +0700

    debugging

--- main.py
+++ main.py
@@ -53,5 +53,5 @@ async def handle_delete():
     return "Deleted successfully"
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
